14c990abb0166eb4bf1e07c16088999789192a03
thistlehouse <fernando2802@gmail.com>
2023-03-04 19:51:02 -0300

    added teste para checagem de n negativo

--- 2-fibonacci.py
+++ 2-fibonacci.py
@@ -1,10 +1,12 @@
 def fibonacci(n):
-    if n == 0:
+    if n < 0:
+        print("n deve ser maior que 0")
+    elif n == 0:
         return 0
-    if n == 1: 
+    elif n == 1: 
         return 1
-    
-    return fibonacci(n-1)+fibonacci(n-2)
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
 
 def imprimeSequecia(n):
     for i in range(0, n):
